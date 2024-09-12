cc3f3864f87fbbcafb6e3129c75304b4703bd454
parvez pappu <mdparveahmedpappu@gmail.com>
2024-09-12 15:01:03 +0600

    added a hello text

--- GuiPractice/Form1.cs
+++ GuiPractice/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuiPractice
@@ -15,6 +8,11 @@ namespace GuiPractice
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello");
         }
     }
 }
